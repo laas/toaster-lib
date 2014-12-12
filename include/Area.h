77ce1be2f1b4166856f2744eab578ab55446dc0a
Gregoire Milliez <gregoire@milliez.fr>
2014-12-12 15:19:41 +0100

    int -> unsigned int

--- include/Area.h
+++ include/Area.h
@@ -12,27 +12,27 @@ class Area
 {
 
 protected:
-    int id;				// each area has a unic id
+    unsigned int id;				// each area has a unic id
     std::string name;		 	// area might have a name
-    int myOwner;		        // Entity related to this area
+    unsigned int myOwner;		        // Entity related to this area
 
 public:
-    std::vector<int> insideEntities_;	  			// Entities present in that area
+    std::vector<unsigned int> insideEntities_;	  			// Entities present in that area
 
     // Constructor
-    Area(int areaId);
+    Area(unsigned int areaId);
 
     // virtual functions
     virtual bool isPointInArea(bg::model::point<double, 2, bg::cs::cartesian> center) = 0;
 
     // functions
-    void removeEntity(int entId);
-    bool isIdInArea(int id);
+    void removeEntity(unsigned int entId);
+    bool isIdInArea(unsigned int id);
 
     // accessors, mutators
-    int getId();
-    void setId(int areaId);
-    int getMyOwner();
+    unsigned int getId();
+    void setId(unsigned int areaId);
+    unsigned int getMyOwner();
     void setMyOwner(int ownerId);
     std::string getName();
     void setName(std::string areaName);
--- src/Area.cpp
+++ src/Area.cpp
@@ -3,17 +3,17 @@
 #include "Area.h"
 
 // Constructor
-Area::Area(int areaId) : id(areaId){
+Area::Area(unsigned int areaId) : id(areaId){
 }
 
 // functions
-void Area::removeEntity(int entId){
-  std::vector<int>::iterator i = std::find(insideEntities_.begin(), insideEntities_.end(), entId);
+void Area::removeEntity(unsigned int entId){
+  std::vector<unsigned int>::iterator i = std::find(insideEntities_.begin(), insideEntities_.end(), entId);
   if (i != insideEntities_.end())
     insideEntities_.erase(i);
 }
 
-bool Area::isIdInArea(int id){
+bool Area::isIdInArea(unsigned int id){
   if( std::find(insideEntities_.begin(), insideEntities_.end(), id) != insideEntities_.end() )
     return true;
   else
@@ -21,15 +21,15 @@ bool Area::isIdInArea(int id){
 }
 
 // accessors, mutators
-int Area::getId(){
+unsigned int Area::getId(){
   return id;
 }
 
-void Area::setId(int areaId){
+void Area::setId(unsigned int areaId){
   id = areaId;
 }
 
-int Area::getMyOwner(){
+unsigned int Area::getMyOwner(){
   return myOwner;
 }
 
