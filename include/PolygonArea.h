5910a248c35a2046aaa79229b5c97bc6961c5e00
Gregoire Milliez <gregoire@milliez.fr>
2015-01-05 19:42:43 +0100

    Bug correction for within polygon

--- include/PolygonArea.h
+++ include/PolygonArea.h
@@ -1,24 +1,22 @@
 // This class define a rectangle area.
 
 #ifndef POLYGON_AREA_H
-  #define POLYGON_AREA_H
+#define POLYGON_AREA_H
 
 
 #include "Area.h"
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/geometries/polygon.hpp>
 
-class PolygonArea : public Area
-{
-
+class PolygonArea : public Area {
 private:
-    
+
 
 public:
     bg::model::polygon<bg::model::d2::point_xy<double> > poly_;
 
     // Constructor
-    PolygonArea(int areaId, double points[][2]);
+    PolygonArea(int areaId, double points[][2], unsigned int polySize);
 
     // Function
     bool isPointInArea(bg::model::point<double, 2, bg::cs::cartesian> point);
--- src/PolygonArea.cpp
+++ src/PolygonArea.cpp
@@ -3,11 +3,12 @@
 #include "PolygonArea.h"
 
 #include <boost/geometry/geometries/adapted/boost_tuple.hpp>
+#include <boost/geometry/geometries/adapted/c_array.hpp>
 
 BOOST_GEOMETRY_REGISTER_BOOST_TUPLE_CS(cs::cartesian)
 
-PolygonArea::PolygonArea(int areaId, double points[][2]) : Area(areaId) {
-    for (unsigned int i = 0; i < sizeof (points) / sizeof (*points); i++) {
+PolygonArea::PolygonArea(int areaId, double points[][2], unsigned int polySize) : Area(areaId) {
+    for (unsigned int i = 0; i < polySize; i++) {
         bg::model::d2::point_xy<double> p(points[i][0], points[i][1]);
         bg::append(poly_, p);
     }
@@ -17,9 +18,6 @@ PolygonArea::PolygonArea(int areaId, double points[][2]) : Area(areaId) {
 
 bool PolygonArea::isPointInArea(bg::model::point<double, 2, bg::cs::cartesian> point) {
     boost::tuple<double, double> p = boost::make_tuple(point.get<0>(), point.get<1>());
-    if (bg::within(p, poly_))
-        return true;
-    else
-        return false;
+    return bg::within(p, poly_);
 }
 
