d0217f4184500163dd03e0e1eb41ecda01b03622
guillaume <guillaum324bis@hotmail.fr>
2017-08-22 18:36:35 +0200

    fix zmin = zmax error and zero hysteresis

--- src/PolygonArea.cpp
+++ src/PolygonArea.cpp
@@ -10,15 +10,15 @@ using namespace std;
 // constructor
 PolygonArea::PolygonArea(int areaId, double points[][2], unsigned int polySize, double zmin, double zmax, double enter, double leave) :
     Area(areaId),
-    enterHysteresis_(enter), leaveHysteresis_(leave)
+    enterHysteresis_(enter + 0.0001), leaveHysteresis_(leave + 0.0001)
 {
     for (unsigned int i = 0; i < polySize; i++) {
         bg::model::d2::point_xy<double> p(points[i][0], points[i][1]);
         bg::append(polyRelative_, p);
         bg::append(poly_, p);
     }
-    z = boost::make_tuple(zmin , zmax);
-    zRelative = boost::make_tuple(zmin , zmax);
+    z = boost::make_tuple(zmin + 0.0001 , zmax + 0.0001);
+    zRelative = boost::make_tuple(zmin + 0.0001 , zmax + 0.0001);
 }
 
 int PolygonArea::isInside(std::vector<point_t> vertex, point_t p)
